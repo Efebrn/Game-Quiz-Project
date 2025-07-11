26f34a7ba2f04bed4dfb8e38a146e9167cc48ec2
Efebrn <effebrn@gmail.com>
2025-07-11 17:40:52 +0300

    Update java.js

--- java.js
+++ java.js
@@ -1,7 +1,7 @@
 // Rastgele Görseller
 const images = [];
 for(let i=1; i<=50; i++) {
-  images.push(`images1/${i}.jpg`);
+  images.push(`photos/${i}.jpg`);
 }
 
 const randomImage = document.getElementById('random-image');
